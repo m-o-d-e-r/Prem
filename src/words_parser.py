bb80d19f5b6bb99617891434e8269f880baac4b9
M_O_D_E_R <bogdan.chernenko.018@gmail.com>
2022-12-05 04:11:32 +0200

    some tests

---
+++ src/words_parser.py
@@ -0,0 +1,12 @@
+import json
+import os
+
+from bs4 import BeautifulSoup
+import requests
+
+
+BASE_PATH = os.path.dirname(os.path.abspath(__file__))
+PATH_TO_CONFIG = BASE_PATH + "/parser.json"
+
+
+
