c3b97801b32e95a3f627716e809a0f7775a769cb
moder <bogdan.chernenko.018@gmail.com>
2023-03-04 20:04:58 +0200

    fix DictReader

--- src/core/config_reader.cpp
+++ src/core/config_reader.cpp
@@ -6,11 +6,9 @@
 
 static void __load_toml_file(toml::table& _table, std::filesystem::path _toml_file)
 {
-    char* _toml_char_path = const_cast<char*>(_toml_file.c_str());
-
-    if (_is_file_exist(_toml_char_path))
+    if (_is_file_exist(_toml_file))
     {
-        _table = toml::parse_file(_toml_char_path);
+        _table = toml::parse_file(_toml_file.c_str());
 
     } else {
         auto __src = fmt::format(
@@ -20,8 +18,8 @@ static void __load_toml_file(toml::table& _table, std::filesystem::path _toml_fi
 
         if (
             _copy_folder(
-                const_cast<char*>(__src.c_str()),
-                const_cast<char*>(_toml_file.parent_path().c_str())
+                std::filesystem::path(__src),
+                std::filesystem::path(_toml_file.parent_path())
             )
         )
         {
@@ -56,28 +54,34 @@ static void __load_toml_file(toml::table& _table, std::filesystem::path _toml_fi
 
 ConfigReader::ConfigReader()
 {
-    char* _home_dir = _get_user_home_dir();
-    strcat(_home_dir, "/.prem");
+    // FIX: 
+    __base_path = __PathObject(_get_user_home_dir()) / ".prem";
 
 
-    if (_is_folder_exist(_home_dir))
+    if (_is_folder_exist(__base_path))
     {
-        namespace fs = std::filesystem;
-
-        fs::path config_path = fs::path(_home_dir) / "config";
-
-        if (_is_folder_exist(const_cast<char*>(config_path.c_str())))
+        __PathObject config_path = getConfigPath();
+        if (_is_folder_exist(config_path))
         {
             __load_toml_file(this->__table["info"], config_path / "info.toml");
             __load_toml_file(this->__table["prem"], config_path /"prem.toml");
 
         } else {
             print_message("\t[-] prem configuration folder is not exist\n", MessageType::ERROR);
-            _copy_folder("src/.prem.bak", _home_dir);
+            _copy_folder("src/.prem.bak", __base_path);
             print_message("\t[+] prem configuration folder was created\n", MessageType::COMPLETED);
 
         }
 
+
+        __PathObject dict_path = getDictPath();
+        if (!_is_folder_exist(dict_path))
+        {
+            print_message("\t[-] prem dict folder is not exist\n", MessageType::ERROR);
+            _copy_folder("src/.prem.bak", __base_path);
+            print_message("\t[+] prem dict folder was created\n", MessageType::COMPLETED);
+
+        }
 
     } else {
         print_message(
@@ -87,8 +91,8 @@ ConfigReader::ConfigReader()
             MessageType::ERROR
         );
 
-        _create_folder(_home_dir);
-        _copy_folder("src/.prem.bak", _home_dir);
+        _create_folder(__base_path);
+        _copy_folder("src/.prem.bak", __base_path);
 
         print_message(
             fmt::format(
@@ -106,3 +110,22 @@ toml::table& ConfigReader::getTableByName(std::string table_name)
 {
     return this->__table[table_name];
 }
+
+
+__PathObject ConfigReader::getBasePath()
+{
+    return __base_path;
+}
+
+
+__PathObject ConfigReader::getConfigPath()
+{
+    return __base_path / "config";
+}
+
+
+__PathObject ConfigReader::getDictPath()
+{
+    return __base_path / "dict";
+}
+
--- src/prem.cpp
+++ src/prem.cpp
@@ -1,31 +1,11 @@
 
-#include <string>
-#include <iostream>
-
-#include <ncurses.h>
-
-#include "interface/BaseWindow.h"
-#include "interface/BufferedWindow.h"
-
-
-#define PREM_KEY_ENTER 13
-#define PREM_KEY_CTRL(x) ((x) & 0x1f)
-
-#include "core/PremStructs/BufferItem.h"
+#include "core/config_reader.h"
 
 #include "core/args_preprocessor.h"
 #include "core/args_router.h"
 
-#include "core/config_reader.h"
-
-#include "core/help_functions.h"
-
-
-//#include <xcb/xcb.h>
-#include "libs/clipboard/include/libclipboard.h"
-#include "libs/args-parser/all.hpp"
-
-
+#define PREM_KEY_ENTER 13
+#define PREM_KEY_CTRL(x) ((x) & 0x1f)
 
 
 
@@ -34,18 +14,15 @@ int main(int argc, char** argv)
     setlocale(LC_ALL, "");
 
 
-    ConfigReader* config = new ConfigReader;
+    ConfigReader config; // read config and check config path
 
-    ArgsPreprocessor __args_preprocessor;
+    ArgsPreprocessor __args_preprocessor; // get arguments from a command line
     __args_preprocessor.parse(argc, argv);
 //    __args_preprocessor.__show_args_map();
 
-
-    ArgsRouter __args_router(__args_preprocessor.getArgsInfo(), config);
+    ArgsRouter __args_router(__args_preprocessor.getArgsInfo(), &config); // run different parts of program
     __args_router.run();
 
-
-    delete config;
 
     return 0;
 
