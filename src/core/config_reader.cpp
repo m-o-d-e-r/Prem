8a8a430dd13a3b40b6fea72af6d11949fa9b6df6
m-o-d-e-r <moder.mode.on@gmail.com>
2024-03-20 17:13:49 +0200

    modify args_*

--- src/core/config_reader.cpp
+++ src/core/config_reader.cpp
@@ -1,11 +1,135 @@
 
 #include "config_reader.h"
+#include "help_functions.h"
+
+
+
+static void __load_toml_file(toml::table& _table, std::filesystem::path _toml_file)
+{
+    char* _toml_char_path = const_cast<char*>(_toml_file.c_str());
+
+    if (_is_file_exist(_toml_char_path))
+    {
+        _table = toml::parse_file(_toml_char_path);
+
+        print_message(
+            fmt::format(
+                "\t[*] config file is loaded: {}\n",
+                _toml_char_path
+            ).c_str(),
+            MessageType::NOTIFICATION
+        );
+
+    } else {
+        auto __src = fmt::format(
+            "src/.prem.bak/config/{}",
+            _toml_file.filename().c_str()
+        );
+
+        if (
+            _copy_folder(
+                const_cast<char*>(__src.c_str()),
+                const_cast<char*>(_toml_file.parent_path().c_str())
+            )
+        )
+        {
+            print_message(
+                fmt::format(
+                    "\t[+] config file [{}] copied FROM {} TO {}\n",
+                    _toml_file.filename().c_str(),
+                    __src,
+                    _toml_file.c_str()
+                ).c_str(),
+                MessageType::COMPLETED
+            );
+        } else {
+            print_message(
+                fmt::format(
+                    "\t[+] error while coping config file [{}] FROM {} TO {}\n",
+                    _toml_file.filename().c_str(),
+                    __src,
+                    _toml_file.c_str()
+                ).c_str(),
+                MessageType::ERROR
+            );
+
+        }
+
+    }
+
+}
+
+
 
 
 ConfigReader::ConfigReader()
 {
-    this->__table["info"] = toml::parse_file("src/config/info.toml");
-    this->__table["prem"] = toml::parse_file("src/config/prem.toml");
+    /*
+    
+        TODO: replace some code to PathChecker
+    
+    */
+
+    char* _home_dir = _get_user_home_dir();
+    strcat(_home_dir, "/.prem");
+
+
+    print_message(
+        fmt::format(
+            "\t[*] prem base folder: {}\n",
+            _home_dir
+        ).c_str(),
+        MessageType::NOTIFICATION
+    );
+
+
+    if (_is_folder_exist(_home_dir))
+    {
+        namespace fs = std::filesystem;
+
+        fs::path config_path = fs::path(_home_dir) / "config";
+
+        print_message(
+            fmt::format(
+                "\t[*] prem base config folder: {}\n\n",
+                config_path.c_str()
+            ).c_str(),
+            MessageType::NOTIFICATION
+        );
+
+
+        if (_is_folder_exist(const_cast<char*>(config_path.c_str())))
+        {
+            __load_toml_file(this->__table["info"], config_path / "info.toml");
+            __load_toml_file(this->__table["prem"], config_path /"prem.toml");
+
+        } else {
+            print_message("\t[-] lol, prem configuration folder is not created\n", MessageType::ERROR);
+            _copy_folder("src/.prem.bak", _home_dir);
+            print_message("\t[+] prem configuration folder was created\n", MessageType::COMPLETED);
+
+        }
+
+
+    } else {
+        print_message(
+            fmt::format(
+                "\t[-] prem base folder is not exist\n"
+            ).c_str(),
+            MessageType::ERROR
+        );
+
+        _create_folder(_home_dir);
+
+        print_message(
+            fmt::format(
+                "\t[-] prem base folder was created\n"
+            ).c_str(),
+            MessageType::COMPLETED
+        );
+
+    }
+
 }
 
 
---
+++ src/core/help_functions.cpp
@@ -0,0 +1,96 @@
+
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <pwd.h>
+#include <map>
+
+#include <fmt/printf.h>
+#include <fmt/color.h>
+
+#include "help_functions.h"
+
+
+static std::map<MessageType, fmt::color> __MESSAGE_COLOR = {
+    {MessageType::ERROR, fmt::color::orange_red},
+    {MessageType::NOTIFICATION, fmt::color::dark_cyan},
+    {MessageType::COMPLETED, fmt::color::lime_green}
+};
+
+
+
+bool _is_folder_exist(char* _path_to_folder)
+{
+    return std::filesystem::is_directory(_path_to_folder);
+}
+
+
+bool _is_file_exist(char* _path_to_file)
+{
+    return std::filesystem::exists(_path_to_file);
+}
+
+
+bool _is_file_exist(const char* _path_to_file)
+{
+    return _is_file_exist(const_cast<char*>(_path_to_file));
+}
+
+
+void _create_folder(char* _path_to_folder)
+{
+    if (!_is_folder_exist(_path_to_folder))
+        std::filesystem::create_directory(_path_to_folder);
+
+}
+
+
+bool _copy_folder(char* _src, char* _dst)
+{
+    namespace fs = std::filesystem;
+    
+    fs::path src(_src);
+    fs::path dst(_dst);
+
+    try
+    {
+        fs::copy(
+            src,
+            dst,
+            fs::copy_options::update_existing
+            | fs::copy_options::recursive
+        );
+        return true;
+    }
+    catch (std::exception& e)
+    {
+        return false;
+    }
+
+}
+
+
+char* _get_user_home_dir()
+{
+    return getpwuid(getuid())->pw_dir;
+}
+
+
+void print_message(char* _text, MessageType _type)
+{
+    fmt::print(
+        fg(__MESSAGE_COLOR[_type]),
+        _text        
+    );
+
+}
+
+
+void print_message(const char* _text, MessageType _type)
+{
+    fmt::print(
+        fg(__MESSAGE_COLOR[_type]),
+        _text        
+    );
+
+}
