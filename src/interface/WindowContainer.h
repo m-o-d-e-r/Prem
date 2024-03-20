bbce6c436e49c965274ddb205c16840514ca60cb
m-o-d-e-r <moder.mode.on@gmail.com>
2024-03-20 17:13:48 +0200

    add new files

--- src/interface/WindowContainer.h
+++ src/interface/WindowContainer.h
@@ -1,12 +1,24 @@
 #ifndef WINDOW_CONTAINER_H
 #define WINDOW_CONTAINER_H
 
+#include <memory>
+#include <vector>
+
 #include "BaseWindow.h"
 
 
 class WindowContainer: public BaseWindow
 {
+private:
+    std::vector<std::shared_ptr<BaseWindow>> __window_objects;
+    int* __window_offsets;
 
+public:
+    WindowContainer();
+    ~WindowContainer();
+
+    void init();
+    void update();
 
 };
 
